7e10e532a30b4262019db3549046a2e43913e9aa
wonsukheo8 <wonsukheo8@gmail.com>
2022-05-08 12:16:54 -0500

    lab1_test

---
+++ Lab1/Add.cpp
@@ -0,0 +1,11 @@
+namespace lab1 
+{
+	int Add(int a, int b)
+	{
+		return a + b;
+	}
+}
+
+
+
+
--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -1,4 +1,15 @@
+#include <iostream>
+
+#include "Add.h"
+
 int main()
 {
+    int a = 10;
+    int b = 15;
 
+    int result = lab1::Add(a, b);
+
+    std::cout << result << std::endl;
+
+    return 0;
 }
