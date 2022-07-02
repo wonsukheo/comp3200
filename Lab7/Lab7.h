877e5e9af8c8f20c605eab204b9a949e567bb1e4
wonsukheo <wonsukheo8@gmail.com>
2022-07-01 22:50:17 -0500

    lab7 half done. worked from main comp

--- Lab7/Lab7.h
+++ Lab7/Lab7.h
@@ -6,7 +6,6 @@
 
 using namespace std;
 
-
 namespace lab7
 {
 	template <typename K, class V>
@@ -31,11 +30,124 @@ namespace lab7
 	{
 		std::vector<K> v;
 
-		for (std::map<K, V>::const_iterator iter = m.begin(); iter != m.end(); ++iter)
+		for (typename std::map<K, V>::const_iterator iter = m.begin(); iter != m.end(); ++iter)
 		{
 			v.push_back(iter->first);
 		}
 
 		return v;
+	}
+
+	template <typename K, class V>
+	std::vector<V> GetValues(const std::map<K, V>& m)
+	{
+		std::vector<V> v;
+
+		for (typename std::map<K, V>::const_iterator iter = m.begin(); iter != m.end(); ++iter)
+		{
+			v.push_back(iter->second);
+		}
+
+		return v;
+	}
+
+	template <typename T>
+	std::vector<T> Reverse(const std::vector<T>& v)
+	{
+		std::vector<T> rv;
+
+		for (typename std::vector<T>::reverse_iterator iter = rv.rend(); iter != rv.rbegin(); --iter)
+		{
+			rv.push_back(*iter);
+		}
+
+		return rv;
+	}
+
+	template <class InputIterator, typename T>
+	InputIterator Find(const InputIterator* begin, const InputIterator* end, const T& value)
+	{
+		while (begin != end)
+		{
+			if (*begin == value)
+			{
+				return begin;
+			}
+
+			++begin;
+		}
+
+		return NULL;
+	}
+
+	template <typename T>
+	std::vector<T> operator+(const std::vector<T>& v1, const std::vector<T>& v2)
+	{
+		std::vector<T> combined;
+
+		for (typename std::vector<T>::const_iterator iter = v1.begin(); iter != v1.end(); ++iter)
+		{
+			if (::Find(combined.begin(), combined.end(), *iter))
+			{
+				combined.push_back(*iter);
+			}
+		}
+
+		for (typename std::vector<T>::const_iterator iter = v2.begin(); iter != v2.end(); ++iter)
+		{
+			if (::Find(combined.begin(), combined.end(), *iter))
+			{
+				combined.push_back(*iter);
+			}
+		}
+
+		return combined;
+	}
+
+	template <typename K, class V>
+	std::map<K, V> operator+(const std::map<K, V>& m1, const std::map<K, V>& m2)
+	{
+		std::map<K, V> combined;
+
+		for (typename std::map<K, V>::const_iterator iter = m1.begin(); iter != m1.end(); ++iter)
+		{
+			if (::Find(combined.begin(), combined.end(), *iter))
+			{
+				combined.insert(*iter);
+			}
+		}
+
+		for (typename std::map<K, V>::const_iterator iter = m2.begin(); iter != m2.end(); ++iter)
+		{
+			if (::Find(combined.begin(), combined.end(), *iter))
+			{
+				combined.insert(*iter);
+			}
+		}
+
+		return combined;
+	}
+
+
+	template <typename T>
+	std::ostream& operator<<(std::ostream& os, const std::vector<T>& v)
+	{
+		for (typename std::vector<T>::const_iterator iter = v.begin(); iter != v.end(); ++iter)
+		{
+			os << ", " << *iter;
+		}
+
+		return os;
+	}
+
+	template <typename K, class V>
+	std::ostream& operator<<(std::ostream& os, const std::map<K, V>& m)
+	{
+		for (typename std::map<K, V>::const_iterator iter = m.begin(); iter != m.end(); ++iter)
+		{
+			os << "{ " << iter->first << ", " << iter->second << " }" << std::endl;
+		}
+
+		return os;
 	}
 }
